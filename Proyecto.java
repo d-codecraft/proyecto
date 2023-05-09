55b1efd132664e48f2ffe70af75a512869c225a2
Juan Forero <113569211+JuanForero09@users.noreply.github.com>
2023-05-08 23:42:02 -0500

    Proyecto
    Corrección de errores, mejoras 1.1

--- Proyecto.java
+++ Proyecto.java
@@ -16,7 +16,7 @@ public class Proyecto extends JFrame implements ActionListener {
     
     public Proyecto() {
         setTitle("Inicio de sesión");
-        setSize(300, 130);
+        setSize(300, 140);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
@@ -49,152 +49,177 @@ public class Proyecto extends JFrame implements ActionListener {
         String password = new String(passwordField.getPassword());
         
         if (isValid(username, password)) {
-            JOptionPane.showMessageDialog(this, "Inicio de sesión exitoso");
 
-            String[] opciones = {"Numeros Primos", "Factorial", "Calculadora", "Contar Vocales", "Tabla de Multiplicar", "Ordenar Numeros"};
-            int seleccion = JOptionPane.showOptionDialog(this, "Seleccione un ejercicio", "Ejercicios", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+            Container container = this.getContentPane();
+            Window window = SwingUtilities.windowForComponent(container);
+            window.dispose();
+
+            boolean Ejercicios = true;
+
+            while (Ejercicios){
+                String[] opciones = {"Números Primos", "Factorial", "Calculadora", "Contar Vocales", "Tabla de Multiplicar", "Ordenar Números"};
+                int seleccion = JOptionPane.showOptionDialog(this, "Seleccione un ejercicio", "Ejercicios", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+                
+                // Dependiendo de la opción seleccionada, iniciar el ejercicio correspondiente
+                if (seleccion == 0) {
+                    //Numeros Primos
+                    // Iniciar Ejercicio 1
+                    // Pedimos al usuario que ingrese un número entero
+                    int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entero:"));
+
+                    boolean esPrimo = true; // suponemos que es primo al principio
+
+                    // comprobamos si el número es divisible por algún otro número diferente a 1 y a sí mismo
+                    for (int i = 2; i < numero; i++) {
+                        if (numero % i == 0) {
+                            esPrimo = false; // si es divisible, no es primo
+                            break;
+                        }
+                    }
+
+                    // mostramos el resultado al usuario
+                    if (esPrimo) {
+                        JOptionPane.showMessageDialog(null, numero + " es un número primo");
+                    } else {
+                        JOptionPane.showMessageDialog(null, numero + " no es un número primo");
+                    }
+
+                    
+
+                } else if (seleccion == 1) {
+                    //Factorial
+                    // Iniciar Ejercicio 2
+                    // Pedimos al usuario que ingrese un número entero positivo
+                    int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entero positivo:"));
+
+                    int factorial = 1; // inicializamos el factorial en 1
+
+                    // calculamos el factorial del número ingresado
+                    for (int i = 1; i <= numero; i++) {
+                        factorial *= i;
+                    }
+
+                    // mostramos el resultado al usuario
+                    JOptionPane.showMessageDialog(null, "El factorial de " + numero + " es " + factorial);
+
+                } else if (seleccion == 2) {
+                    //Calculadora
+                    // Iniciar Ejercicio 3
+                    // Pedimos al usuario que ingrese dos números
+                    double numero1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el primer número:"));
+                    double numero2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el segundo número:"));
+
+                    // Pedimos al usuario que elija la operación a realizar
+                    String[] opcione = {"Sumar", "Restar", "Multiplicar", "Dividir"};
+                    int seleccio = JOptionPane.showOptionDialog(null, "Seleccione la operación a realizar:", "Calculadora",
+                            JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcione, opcione[0]);
+
+                    double resultado = 0; // inicializamos el resultado en 0
+
+                    // realizamos la operación correspondiente
+                    switch (seleccio) {
+                        case 0: // suma
+                            resultado = numero1 + numero2;
+                            break;
+                        case 1: // resta
+                            resultado = numero1 - numero2;
+                            break;
+                        case 2: // multiplicación
+                            resultado = numero1 * numero2;
+                            break;
+                        case 3: // división
+                            resultado = numero1 / numero2;
+                            break;
+                    }
+
+                    // mostramos el resultado al usuario
+                    JOptionPane.showMessageDialog(null, "El resultado es " + resultado);
+
+
+                } else if (seleccion == 3) {
+                    //Contar Vocales
+                    // Iniciar Ejercicio 4
+                    // Pedimos al usuario que ingrese una cadena de caracteres
+                    String cadena = JOptionPane.showInputDialog("Ingrese una cadena de caracteres:");
+
+                    int contadorVocales = 0; // inicializamos el contador de vocales en 0
+
+                    // recorremos la cadena de caracteres
+                    for (int i = 0; i < cadena.length(); i++) {
+                        // comprobamos si el caracter actual es una vocal
+                        char caracterActual = cadena.charAt(i);
+                        if (caracterActual == 'a' || caracterActual == 'e' || caracterActual == 'i' || caracterActual == 'o' || caracterActual == 'u' ||
+                                caracterActual == 'A' || caracterActual == 'E' || caracterActual == 'I' || caracterActual == 'O' || caracterActual == 'U') {
+                            contadorVocales++; // si es una vocal, aumentamos el contador
+                        }
+                    }
+
+                    // mostramos el resultado al usuario
+                    JOptionPane.showMessageDialog(null, "La cadena de caracteres ingresada tiene " + contadorVocales + " vocales");
+
+
+                } else if (seleccion == 4) {
+                    //Tabla de Multiplicar
+                    // Iniciar Ejercicio 5
+                    String tabla = ""; // inicializamos la tabla en una cadena vacía
+
+                    // realizamos la tabla de multiplicar para los números del 1 al 10
+                    for (int i = 1; i <= 10; i++) {
+                        for (int j = 1; j <= 10; j++) {
+                            int resultado = i * j;
+                            tabla += i + " x " + j + " = " + resultado + "\n";
+                        }
+                        tabla += "\n"; // agregamos una línea en blanco para separar las tablas de cada número
+                    }
             
-            // Dependiendo de la opción seleccionada, iniciar el ejercicio correspondiente
-            if (seleccion == 0) {
-                //Numeros Primos
-                // Iniciar Ejercicio 1
-                 // Pedimos al usuario que ingrese un número entero
-                int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entero:"));
+                    // mostramos la tabla al usuario
+                    JOptionPane.showMessageDialog(null, tabla);
 
-                boolean esPrimo = true; // suponemos que es primo al principio
+                } else if (seleccion == 5) {
+                    // Ordenar Numeros
+                    // Iniciar Ejercicio 6
+                    // Pedimos al usuario que ingrese la cantidad de números que desea generar
+                    int cantidadNumeros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de números que desea generar:"));
 
-                // comprobamos si el número es divisible por algún otro número diferente a 1 y a sí mismo
-                for (int i = 2; i < numero; i++) {
-                    if (numero % i == 0) {
-                        esPrimo = false; // si es divisible, no es primo
-                        break;
+                    int[] numeros = new int[cantidadNumeros]; // creamos un arreglo para almacenar los números
+
+                    // Generamos números aleatorios y los almacenamos en el arreglo
+                    Random rand = new Random();
+                    for (int i = 0; i < cantidadNumeros; i++) {
+                        numeros[i] = rand.nextInt(100); // generamos números aleatorios entre 0 y 99
                     }
-                }
 
-                // mostramos el resultado al usuario
-                if (esPrimo) {
-                    JOptionPane.showMessageDialog(null, numero + " es un número primo");
-                } else {
-                    JOptionPane.showMessageDialog(null, numero + " no es un número primo");
-                }
+                    // Ordenamos el arreglo de menor a mayor
+                    Arrays.sort(numeros);
 
-            } else if (seleccion == 1) {
-                //Factorial
-                // Iniciar Ejercicio 2
-                 // Pedimos al usuario que ingrese un número entero positivo
-                int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entero positivo:"));
-
-                int factorial = 1; // inicializamos el factorial en 1
-
-                // calculamos el factorial del número ingresado
-                for (int i = 1; i <= numero; i++) {
-                    factorial *= i;
-                }
-
-                // mostramos el resultado al usuario
-                JOptionPane.showMessageDialog(null, "El factorial de " + numero + " es " + factorial);
-
-            } else if (seleccion == 2) {
-                //Calculadora
-                // Iniciar Ejercicio 3
-                // Pedimos al usuario que ingrese dos números
-                double numero1 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el primer número:"));
-                double numero2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el segundo número:"));
-
-                // Pedimos al usuario que elija la operación a realizar
-                String[] opcione = {"Sumar", "Restar", "Multiplicar", "Dividir"};
-                int seleccio = JOptionPane.showOptionDialog(null, "Seleccione la operación a realizar:", "Calculadora",
-                        JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcione, opcione[0]);
-
-                double resultado = 0; // inicializamos el resultado en 0
-
-                // realizamos la operación correspondiente
-                switch (seleccio) {
-                    case 0: // suma
-                        resultado = numero1 + numero2;
-                        break;
-                    case 1: // resta
-                        resultado = numero1 - numero2;
-                        break;
-                    case 2: // multiplicación
-                        resultado = numero1 * numero2;
-                        break;
-                    case 3: // división
-                        resultado = numero1 / numero2;
-                        break;
-                }
-
-                // mostramos el resultado al usuario
-                JOptionPane.showMessageDialog(null, "El resultado es " + resultado);
-
-
-            } else if (seleccion == 3) {
-                //Contar Vocales
-                // Iniciar Ejercicio 4
-                 // Pedimos al usuario que ingrese una cadena de caracteres
-                String cadena = JOptionPane.showInputDialog("Ingrese una cadena de caracteres:");
-
-                int contadorVocales = 0; // inicializamos el contador de vocales en 0
-
-                // recorremos la cadena de caracteres
-                for (int i = 0; i < cadena.length(); i++) {
-                    // comprobamos si el caracter actual es una vocal
-                    char caracterActual = cadena.charAt(i);
-                    if (caracterActual == 'a' || caracterActual == 'e' || caracterActual == 'i' || caracterActual == 'o' || caracterActual == 'u' ||
-                            caracterActual == 'A' || caracterActual == 'E' || caracterActual == 'I' || caracterActual == 'O' || caracterActual == 'U') {
-                        contadorVocales++; // si es una vocal, aumentamos el contador
+                    // Mostramos los números ordenados al usuario
+                    String numerosOrdenados = "Los números generados y ordenados son:\n";
+                    for (int i = 0; i < cantidadNumeros; i++) {
+                        numerosOrdenados += numeros[i] + "\n";
                     }
+
+                    JOptionPane.showMessageDialog(null, numerosOrdenados);
+
                 }
 
-                // mostramos el resultado al usuario
-                JOptionPane.showMessageDialog(null, "La cadena de caracteres ingresada tiene " + contadorVocales + " vocales");
-
-
-            } else if (seleccion == 4) {
-                //Tabla de Multiplicar
-                // Iniciar Ejercicio 5
-                String tabla = ""; // inicializamos la tabla en una cadena vacía
-
-                // realizamos la tabla de multiplicar para los números del 1 al 10
-                for (int i = 1; i <= 10; i++) {
-                    for (int j = 1; j <= 10; j++) {
-                        int resultado = i * j;
-                        tabla += i + " x " + j + " = " + resultado + "\n";
-                    }
-                    tabla += "\n"; // agregamos una línea en blanco para separar las tablas de cada número
-                }
-        
-                // mostramos la tabla al usuario
-                JOptionPane.showMessageDialog(null, tabla);
-
-            } else if (seleccion == 5) {
-                // Ordenar Numeros
-                // Iniciar Ejercicio 6
-                // Pedimos al usuario que ingrese la cantidad de números que desea generar
-                int cantidadNumeros = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de números que desea generar:"));
-
-                int[] numeros = new int[cantidadNumeros]; // creamos un arreglo para almacenar los números
-
-                // Generamos números aleatorios y los almacenamos en el arreglo
-                Random rand = new Random();
-                for (int i = 0; i < cantidadNumeros; i++) {
-                    numeros[i] = rand.nextInt(100); // generamos números aleatorios entre 0 y 99
+                 // Mostrar un cuadro de diálogo preguntando si el usuario desea continuar o salir del programa
+                int respuesta = JOptionPane.showConfirmDialog(this, "¿Desea realizar otro ejercicio?", "Continuar", JOptionPane.YES_NO_OPTION);
+                if (respuesta == JOptionPane.NO_OPTION) {
+                    Ejercicios = false;
                 }
 
-                // Ordenamos el arreglo de menor a mayor
-                Arrays.sort(numeros);
 
-                // Mostramos los números ordenados al usuario
-                String numerosOrdenados = "Los números generados y ordenados son:\n";
-                for (int i = 0; i < cantidadNumeros; i++) {
-                    numerosOrdenados += numeros[i] + "\n";
-                }
+            }
 
-                JOptionPane.showMessageDialog(null, numerosOrdenados);
+             
 
-            } 
+        // Definir una variable para controlar si el programa debe seguir ejecutándose
+                
 
-            //Termina los ejercios
+        //Despedida al finalizar el programa
+        JOptionPane.showMessageDialog(this, "¡Gracias por utilizar el programa!", "Adiós", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+
 
         } else {
             JOptionPane.showMessageDialog(this, "Credenciales inválidas");
@@ -204,7 +229,7 @@ public class Proyecto extends JFrame implements ActionListener {
     private boolean isValid(String username, String password) {
         // Lógica de autenticación aquí
         // Devuelve true si las credenciales son válidas, false de lo contrario
-        return username.equals("david") && password.equals("1234");
+        return username.equals("admin") && password.equals("admin123");
     }
     
     public static void main(String[] args) {
